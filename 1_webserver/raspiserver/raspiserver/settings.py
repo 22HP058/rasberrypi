44997ac871b10b5e45694f8da6e413c4c67c5435
seb <sbscsue@gmail.com>
2022-09-11 02:11:36 +0900

    0910_web 테이블 모두 추가 , date time 상위 50개 만뽑아 오기 + tram의 경우 중복 이슈로 인한 int primary 설정 추후 변경할지 생각

--- 1_webserver/raspiserver/raspiserver/settings.py
+++ 1_webserver/raspiserver/raspiserver/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sensor',
+    'tram',
     'rest_framework',
     'drf_yasg',
 ]
--- 1_webserver/raspiserver/raspiserver/urls.py
+++ 1_webserver/raspiserver/raspiserver/urls.py
@@ -37,7 +37,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('sensor.urls')),
+    path('sensor/',include('sensor.urls')),
+    path('tram/',include('tram.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    
