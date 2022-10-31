e78cf9cfa77637ffc80b455e362574af47d086f5
seb <sbscsue@gmail.com>
2022-11-01 00:57:15 +0900

    1031_add station 사람 혼잡도 판단_statioon proto 정리

---
+++ 0_module/11_tensorflow/01_edje/object_station/sentFirebase.py
@@ -0,0 +1,41 @@
+#seb : plus 
+#multiprojecessing
+from multiprocessing.connection import Listener
+
+#firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+#for firebase format
+from datetime import datetime
+
+
+
+cred = credentials.Certificate("//home//pi//File//tram-e65c4-firebase-adminsdk-mguas-51f6bc11fd.json")
+firebase_admin.initialize_app(cred,{'databaseURL' : "https://tram-e65c4-default-rtdb.firebaseio.com//"})
+
+ref = db.reference("stationCongestion/") #path
+
+
+address = ('localhost',7000)
+listener = Listener(address,authkey=b"pi")
+conn = listener.accept()
+print("socket connection:",listener.last_accepted)
+
+
+
+while True: 
+        print("<------TRIGGER : CHANGE PEOPLE CONGESTION -> SEND FIREBASE DB------>")
+        peopleCnt = conn.recv()
+    
+        #seb : current time
+        currentTime = datetime.now() 
+        parsingCurrentTime = currentTime.strftime('%Y-%m-%d %H:%M:%S')
+
+        #seb : firebase append
+        print(parsingCurrentTime,"\t",peopleCnt)
+        ref.update({parsingCurrentTime:peopleCnt})
+
+
+
+
